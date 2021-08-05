0db274c8bb9f95111f95890103532c598ad3c6b5
ojsilvera <ojsilvera@gmail.com>
2021-08-05 12:58:25 -0500

    calculando media gometrica y medianas

---
+++ assets/documents/descPromedioMedia.md
@@ -0,0 +1,7 @@
+## estructura de un for each que suma los elementos de un array
+
+var numebers = [0, 1, 2];
+
+numbers.forEach( function(valor, indice, array) {
+sumatoria = sumatoria + valor;
+});
---
+++ js/promedio_moda_medi.js
@@ -0,0 +1,81 @@
+function promedio(lista){
+
+    var sumatoria = 0;
+
+    for(var i = 0; i < lista.length; i++){
+        var sumatoria = sumatoria + lista[i];
+    }
+
+    // var sumatoria = lista.reduce(
+    //     function(vlrAcumulado = 0, vlrNuevo){
+    //         return vlrAcumulado + vlrNuevo;
+    //     }
+    // );
+
+    var promedioL = sumatoria / lista.length;
+
+    return promedioL;
+}
+
+//funcion media
+
+//determina si la media es para para o impar
+function par(numbers){
+
+    if(numbers.length % 2 === 0){
+        return true;
+    }else{
+        return false;
+    }
+
+}
+
+//calcula la media
+function calcularMedia(numbers){
+
+    if(par(numbers)){
+
+        var mediaP = (numbers.length/2) - 1;
+        var val1 = numbers[mediaP];
+        var val2 = numbers.reverse()[mediaP];
+        promedioP = (val1 + val2)/2;
+        return promedioP;
+
+    }else{
+        var mediaI = parseInt(numbers.length/2);
+        promedioI = numbers[mediaI];
+        return promedioI;
+    }
+}
+
+//recibe y formate la cadena que viene del html
+
+function stringToArray(){
+
+    // obtiene la dadena del input
+    var cadena = document.getElementById("arreglo");
+    // extrae el valor de lo enviado desde html
+    var cadenaInput = cadena.value;
+    // separador determinado
+    var separador = ",";
+    //convierte la cadena en un array
+    var numbersT = cadenaInput.split(separador);
+    //establece el tipo de variable en un array.
+    var numbers = [];
+    // convierte cada valor del array de string a numeros
+    numbersT.forEach( function(valor, indice, array) {
+
+        var dato = parseInt(valor);
+        numbers.push(dato);
+
+    });
+
+    //envia el array de numeros a la funcion que debe calcular la media
+    totalPromedio = calcularMedia(numbers);
+
+    //muestra el valor del promedio en un p en html
+    var promedioHTML = document.getElementById("promedio");
+    promedio.innerText = "el promedio es: " + totalPromedio;
+
+}
+
